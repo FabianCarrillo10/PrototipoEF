d14109c7a7e5c590faeb9e3d54dddcd8ad01f62b
FabianCarrillo10 <fabiancarrillo20011004@gmail.com>
2022-11-15 13:41:34 -0600

    Mantenimientos

---
+++ Codigo/Modulos/Hotel/Vista/Marca.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace ComprasVista
+{
+    partial class Marca
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.navegador1 = new NavegadorVista.Navegador();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(64, 399);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(792, 150);
+            this.dataGridView1.TabIndex = 27;
+            this.dataGridView1.Tag = "marca";
+            // 
+            // navegador1
+            // 
+            this.navegador1.Location = new System.Drawing.Point(159, -2);
+            this.navegador1.Name = "navegador1";
+            this.navegador1.Size = new System.Drawing.Size(574, 152);
+            this.navegador1.TabIndex = 26;
+            this.navegador1.Load += new System.EventHandler(this.navegador1_Load);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(417, 173);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 20;
+            this.textBox4.Tag = "descripcion";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(332, 173);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "descripcion";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(204, 204);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 17;
+            this.textBox2.Tag = "nombre";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(204, 173);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 16;
+            this.textBox1.Tag = "idMarca";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(119, 204);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Nombre";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(119, 173);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "id";
+            // 
+            // Marca
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(921, 547);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.navegador1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Marca";
+            this.Text = "Marca";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private NavegadorVista.Navegador navegador1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Codigo/Modulos/Hotel/Vista/central_Compra.Designer.cs
@@ -0,0 +1,248 @@
+﻿
+namespace ComprasVista
+{
+    partial class central_Compra
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelSideMenu = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnReportes = new System.Windows.Forms.Button();
+            this.panelClientes = new System.Windows.Forms.Panel();
+            this.btnProcesos = new System.Windows.Forms.Button();
+            this.panelCompras = new System.Windows.Forms.Panel();
+            this.btnEmpresa = new System.Windows.Forms.Button();
+            this.btnMarca = new System.Windows.Forms.Button();
+            this.btnCatalogo = new System.Windows.Forms.Button();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panelSideMenu.SuspendLayout();
+            this.panelCompras.SuspendLayout();
+            this.panelLogo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelSideMenu
+            // 
+            this.panelSideMenu.AutoScroll = true;
+            this.panelSideMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(164)))), ((int)(((byte)(242)))));
+            this.panelSideMenu.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.panelSideMenu.Controls.Add(this.panel1);
+            this.panelSideMenu.Controls.Add(this.btnReportes);
+            this.panelSideMenu.Controls.Add(this.panelClientes);
+            this.panelSideMenu.Controls.Add(this.btnProcesos);
+            this.panelSideMenu.Controls.Add(this.panelCompras);
+            this.panelSideMenu.Controls.Add(this.btnCatalogo);
+            this.panelSideMenu.Controls.Add(this.panelLogo);
+            this.panelSideMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelSideMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelSideMenu.Name = "panelSideMenu";
+            this.panelSideMenu.Size = new System.Drawing.Size(237, 749);
+            this.panelSideMenu.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(215)))), ((int)(((byte)(249)))));
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 984);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(220, 260);
+            this.panel1.TabIndex = 5;
+            // 
+            // btnReportes
+            // 
+            this.btnReportes.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnReportes.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnReportes.FlatAppearance.BorderSize = 0;
+            this.btnReportes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReportes.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReportes.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(242)))), ((int)(((byte)(253)))));
+            this.btnReportes.Location = new System.Drawing.Point(0, 945);
+            this.btnReportes.Name = "btnReportes";
+            this.btnReportes.Padding = new System.Windows.Forms.Padding(51, 0, 0, 0);
+            this.btnReportes.Size = new System.Drawing.Size(220, 39);
+            this.btnReportes.TabIndex = 4;
+            this.btnReportes.Text = "REPORTES";
+            this.btnReportes.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnReportes.UseVisualStyleBackColor = true;
+            this.btnReportes.Click += new System.EventHandler(this.btnProveedor_Click);
+            // 
+            // panelClientes
+            // 
+            this.panelClientes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(215)))), ((int)(((byte)(249)))));
+            this.panelClientes.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelClientes.Location = new System.Drawing.Point(0, 430);
+            this.panelClientes.Name = "panelClientes";
+            this.panelClientes.Size = new System.Drawing.Size(220, 515);
+            this.panelClientes.TabIndex = 3;
+            // 
+            // btnProcesos
+            // 
+            this.btnProcesos.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnProcesos.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnProcesos.FlatAppearance.BorderSize = 0;
+            this.btnProcesos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnProcesos.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProcesos.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(242)))), ((int)(((byte)(253)))));
+            this.btnProcesos.Location = new System.Drawing.Point(0, 391);
+            this.btnProcesos.Name = "btnProcesos";
+            this.btnProcesos.Padding = new System.Windows.Forms.Padding(51, 0, 0, 0);
+            this.btnProcesos.Size = new System.Drawing.Size(220, 39);
+            this.btnProcesos.TabIndex = 2;
+            this.btnProcesos.Text = "PROCESOS";
+            this.btnProcesos.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnProcesos.UseVisualStyleBackColor = true;
+            this.btnProcesos.Click += new System.EventHandler(this.btnClient_Click);
+            // 
+            // panelCompras
+            // 
+            this.panelCompras.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(215)))), ((int)(((byte)(249)))));
+            this.panelCompras.Controls.Add(this.btnEmpresa);
+            this.panelCompras.Controls.Add(this.btnMarca);
+            this.panelCompras.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelCompras.Location = new System.Drawing.Point(0, 126);
+            this.panelCompras.Name = "panelCompras";
+            this.panelCompras.Size = new System.Drawing.Size(220, 265);
+            this.panelCompras.TabIndex = 1;
+            // 
+            // btnEmpresa
+            // 
+            this.btnEmpresa.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnEmpresa.FlatAppearance.BorderSize = 0;
+            this.btnEmpresa.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEmpresa.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.btnEmpresa.Location = new System.Drawing.Point(0, 35);
+            this.btnEmpresa.Name = "btnEmpresa";
+            this.btnEmpresa.Padding = new System.Windows.Forms.Padding(64, 0, 0, 0);
+            this.btnEmpresa.Size = new System.Drawing.Size(220, 35);
+            this.btnEmpresa.TabIndex = 2;
+            this.btnEmpresa.Text = "Empresa";
+            this.btnEmpresa.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEmpresa.UseVisualStyleBackColor = true;
+            this.btnEmpresa.Click += new System.EventHandler(this.btnOrdenes_Click);
+            // 
+            // btnMarca
+            // 
+            this.btnMarca.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnMarca.FlatAppearance.BorderSize = 0;
+            this.btnMarca.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMarca.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.btnMarca.Location = new System.Drawing.Point(0, 0);
+            this.btnMarca.Name = "btnMarca";
+            this.btnMarca.Padding = new System.Windows.Forms.Padding(64, 0, 0, 0);
+            this.btnMarca.Size = new System.Drawing.Size(220, 35);
+            this.btnMarca.TabIndex = 0;
+            this.btnMarca.Text = "Marca";
+            this.btnMarca.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMarca.UseVisualStyleBackColor = true;
+            this.btnMarca.Click += new System.EventHandler(this.btnCompra_Click);
+            // 
+            // btnCatalogo
+            // 
+            this.btnCatalogo.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnCatalogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnCatalogo.FlatAppearance.BorderSize = 0;
+            this.btnCatalogo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCatalogo.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCatalogo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(242)))), ((int)(((byte)(253)))));
+            this.btnCatalogo.Location = new System.Drawing.Point(0, 87);
+            this.btnCatalogo.Name = "btnCatalogo";
+            this.btnCatalogo.Padding = new System.Windows.Forms.Padding(51, 0, 0, 0);
+            this.btnCatalogo.Size = new System.Drawing.Size(220, 39);
+            this.btnCatalogo.TabIndex = 0;
+            this.btnCatalogo.Text = "CATALOGOS";
+            this.btnCatalogo.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCatalogo.UseVisualStyleBackColor = true;
+            this.btnCatalogo.Click += new System.EventHandler(this.btnCompras_Click);
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.Controls.Add(this.pictureBox1);
+            this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(220, 87);
+            this.panelLogo.TabIndex = 0;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Image = global::ComprasVista.Properties.Resources.Hotel;
+            this.pictureBox2.Location = new System.Drawing.Point(237, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(1027, 749);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(220, 87);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // central_Compra
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 749);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.panelSideMenu);
+            this.IsMdiContainer = true;
+            this.MinimumSize = new System.Drawing.Size(1280, 766);
+            this.Name = "central_Compra";
+            this.Text = "Hoteleria";
+            this.panelSideMenu.ResumeLayout(false);
+            this.panelCompras.ResumeLayout(false);
+            this.panelLogo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelSideMenu;
+        private System.Windows.Forms.Panel panelClientes;
+        private System.Windows.Forms.Button btnProcesos;
+        private System.Windows.Forms.Panel panelCompras;
+        private System.Windows.Forms.Button btnEmpresa;
+        private System.Windows.Forms.Button btnMarca;
+        private System.Windows.Forms.Button btnCatalogo;
+        private System.Windows.Forms.Panel panelLogo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnReportes;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
